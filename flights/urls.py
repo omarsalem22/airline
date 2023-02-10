40a553962fd1da7213b8e67fb956ba2071fe2e53
omarsalem <omarmohamed25112001@gamil.com>
2023-02-10 13:43:19 +0200

    users

--- flights/urls.py
+++ flights/urls.py
@@ -3,6 +3,8 @@ from . import views
 urlpatterns=[
 
     path("index",views.index,name="index"),
-    path("<int:flight_id>",views.flight,name="fligth")
+    path("<int:flight_id>",views.flight,name="fligth"),
+    path("<int:flight_id>/book",views.book ,name="book")
+
 ]
 
--- flights/views.py
+++ flights/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from  .models import Flight ,Airport
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from  .models import Flight ,Airport,Passenger
 
 # Create your views here.
 def index(request):
@@ -12,5 +14,13 @@ def index(request):
 def  flight(request,flight_id):
     flight=Flight.objects.get(pk=flight_id)
     passengers=flight.passengers.all()
-    context={"fligth":flight,"passengers":passengers}
+    context={"fligth":flight,"passengers":passengers,"non_passengers":Passenger.objects.exclude(flights=flight).all()}
     return render (request,"fligths/flight.html",context)
+
+def book(request,flight_id):
+    if request.method=="POST":
+        flight=Flight.objects.get(pk=flight_id)
+        passenger=Passenger.objects.get(pk=int(request.POST["passenger"]))
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse('fligth',args=(flight.id,)))
+        
